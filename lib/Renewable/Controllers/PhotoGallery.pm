ba5ae0069c4bea188a9309e6b64b1e1677995245
root <root@ubuntu.ubuntu-domain>
2013-07-09 01:07:00 +0300

    Added dynamic content to photo gallery

--- lib/Renewable/Controllers/PhotoGallery.pm
+++ lib/Renewable/Controllers/PhotoGallery.pm
@@ -3,9 +3,19 @@ use Mojo::Base 'Mojolicious::Controller';
 
 sub index {
   my $self = shift;
-
+  
+  my $dir = "/home/tudor/projects/renewable/public/assets/img/photo_gallery";
+  
+  opendir DIR, $dir or die "cannot open dir $dir: $!";
+  my @photo_files = readdir DIR; 
+  closedir DIR;
+  
+  @photo_files = grep { ( $_ ne '.' ) and ( $_ ne '..' ) } @photo_files;
+  
   $self->render(
-    template => 'views/photogallery');
+      photos => [@photo_files],
+      template => 'views/photogallery'
+    );
 }
 
 1;
